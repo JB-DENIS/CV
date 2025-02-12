38480508284281bd80fad7b7248514fc69d5f275
jdenis-insn <jean-benoit.denis@tdk.com>
2025-02-12 08:39:05 +0100

    Revert "center picture"
    This reverts commit 744fd2e7052b5fea69b2e70bb3f54a063736ac73.

--- main.py
+++ main.py
@@ -38,15 +38,7 @@ def catego(option):
 # Display :
 st.set_page_config(page_title="CV JB DENIS", layout="wide", page_icon="img/profil.jpg")
 st.markdown("""<a id="top"></a>""", unsafe_allow_html=True)
-# st.sidebar.image("img/profil.jpg", width=220)
-st.sidebar.markdown(
-    f"""
-    <div style="text-align: center;">
-        <img src="img/profil.jpg" width="220">
-    </div>
-    """,
-    unsafe_allow_html=True,
-)
+st.sidebar.image("img/profil.jpg", width=220)
 st.sidebar.title("Jean-Benoît DENIS, Ph.D")
 st.sidebar.subheader("Ingénieur Data Scientist")
 
